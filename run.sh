29fd76d3cb4b18ca5cdcd41e93d12ddb41538c69
Elizabeth Oladapo <fatanmielizabeth@yahoo.com>
2025-02-15 19:32:46 +0000

    removed config.py and renamed database.py to connect_database.py

--- run.sh
+++ run.sh
@@ -3,34 +3,30 @@
 # Stop script on error
 set -e
 
-echo "🚀 Setting up the project..."
+echo "Setting up the project..."
 
 # Create virtual environment if not exists
 if [ ! -d "venv" ]; then
-    echo "📦 Creating virtual environment..."
+    echo "Creating virtual environment..."
     python -m venv venv
 fi
 
 # Activate virtual environment
-echo "🔧 Activating virtual environment..."
+echo "Activating virtual environment..."
 source venv/Scripts/activate
 
 # Check if required dependencies are installed
 if ! pip freeze | grep -q -F -f backend/requirements.txt; then
-    echo "📦 Installing dependencies..."
+    echo "Installing dependencies..."
     pip install -r backend/requirements.txt
 else
-    echo "✅ All dependencies are already installed."
+    echo "All dependencies are already installed."
 fi
 
-# Run database migration
-#echo "🛠️ Creating PostgreSQL tables..."
-python backend/models.py
-
 # Populate database and Redis cache
-#echo "📥 Populating PostgreSQL and Redis..."
+echo "Creating PostgreSQL tables..., Populating PostgreSQL and Redis..."
 python backend/populate_db.py
 
 # Start Flask API
-echo "🚀 Starting Flask application..."
+echo "Starting Flask application..."
 python backend/app.py
